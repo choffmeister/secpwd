e2c4a6d8d30e04952ea5a31567d1d900dec71eb9
Christian Hoffmeister <mail@choffmeister.de>
2013-11-22 23:40:13 +0100

    Add AES en-/decryption functions

--- src/main/scala/de/choffmeister/secpwd/CryptoUtils.scala
+++ src/main/scala/de/choffmeister/secpwd/CryptoUtils.scala
@@ -1,11 +1,14 @@
 package de.choffmeister.secpwd
 
+import java.io._
 import javax.crypto._
 import javax.crypto.spec._
 import java.security._
 import java.security.spec._
 
 object CryptoUtils {
+  private lazy val random = new SecureRandom()
+
   /**
    * Derives an encryption key according to PBKDF2 defined in PKCS#5 v2.0.
    * For more information see http://tools.ietf.org/html/rfc2898.
@@ -22,5 +25,40 @@ object CryptoUtils {
    */
   def deriveAesKey(passPhrase: Array[Char], salt: Array[Byte], iterationCount: Int = 1024 * 128): SecretKeySpec = {
     new SecretKeySpec(deriveKey(passPhrase, salt, iterationCount, 256), "AES")
+  }
+
+  /**
+   * Generates a cipher stream with AES/CBC/PKCS5Padding algorithm. Uses PBKDF2 to derive encryption key from pass phrase.
+   */
+  def encryptAes(output: OutputStream, passPhrase: Array[Char], salt: Array[Byte], iterationCount: Int, iv: Array[Byte])(inner: OutputStream => Any) {
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    val key = deriveAesKey(passPhrase, salt, iterationCount)
+    cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(iv))
+    val cipherStream = new CipherOutputStream(output, cipher)
+    inner(cipherStream)
+    cipherStream.flush()
+    cipherStream.close()
+  }
+
+  /**
+   * Generates a cipher stream with AES/CBC/PKCS5Padding algorithm. Uses PBKDF2 to derive encryption key from pass phrase.
+   */
+  def decryptAes(input: InputStream, passPhrase: Array[Char], salt: Array[Byte], iterationCount: Int, iv: Array[Byte])(inner: InputStream => Any) {
+    val cipher = Cipher.getInstance("AES/CBC/PKCS5Padding")
+    val key = deriveAesKey(passPhrase, salt, iterationCount)
+    cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv))
+    val cipherStream = new CipherInputStream(input, cipher)
+    inner(cipherStream)
+    cipherStream.close()
+  }
+
+  def generateRandomOctets(length: Int): Array[Byte] = {
+    val buffer = new Array[Byte](length)
+    random.nextBytes(buffer)
+    buffer
+  }
+
+  def generateRandomOctets(buffer: Array[Byte]) {
+    random.nextBytes(buffer)
   }
 }
--- src/test/scala/de/choffmeister/secpwd/CryptoUtilsSpec.scala
+++ src/test/scala/de/choffmeister/secpwd/CryptoUtilsSpec.scala
@@ -4,10 +4,11 @@ import org.specs2.mutable._
 import org.specs2.runner.JUnitRunner
 import org.junit.runner.RunWith
 import org.apache.commons.codec.binary.Hex
+import java.io._
 
 @RunWith(classOf[JUnitRunner])
 class CryptoUtilsSpec extends Specification {
-  "deriveKey" in {
+  "derive keys" in {
     // test vectors from http://tools.ietf.org/html/rfc6070
     val key1 = CryptoUtils.deriveKey("password".toCharArray, "salt".getBytes("ASCII"), 1, 20 * 8)
     key1.toSeq === Hex.decodeHex("0c 60 c8 0f 96 1f 0e 71 f3 a9 b5 24 af 60 12 06 2f e0 37 a6".replaceAll(" ", "").toCharArray).toSeq
@@ -17,8 +18,68 @@ class CryptoUtilsSpec extends Specification {
     key3.toSeq === Hex.decodeHex("56 fa 6a a7 55 48 09 9d cc 37 d7 f0 34 25 e0 c3".replaceAll(" ", "").toCharArray).toSeq
   }
   
-  "deriveAesKey" in {
+  "derive AES keys" in {
     val key = CryptoUtils.deriveAesKey("super-secret".toCharArray, "epic-random".getBytes("ASCII"))
     key.getEncoded must haveSize(256 / 8)
+  }
+
+  "generate random octets" in {
+    val rnd1 = new Array[Byte](24)
+    val rnd2 = new Array[Byte](24)
+    CryptoUtils.generateRandomOctets(rnd1)
+    CryptoUtils.generateRandomOctets(rnd2)
+    val rnd3 = CryptoUtils.generateRandomOctets(24)
+    val rnd4 = CryptoUtils.generateRandomOctets(24)
+
+    rnd1 !== rnd2
+    rnd1 !== rnd3
+    rnd1 !== rnd4
+    rnd2 !== rnd3
+    rnd2 !== rnd4
+    rnd3 !== rnd4
+  }
+
+  "encrypt and decrypt with AES" in {
+    val passPhrase = "secure-password".toCharArray
+    val salt = CryptoUtils.generateRandomOctets(8)
+    val iterationCount = 512
+    val iv = CryptoUtils.generateRandomOctets(16)
+
+    val encryptedWrite = new ByteArrayOutputStream()
+    CryptoUtils.encryptAes(encryptedWrite, passPhrase, salt, iterationCount, iv)(writeString(_, "Hello World!"))
+    val encryptedRead = new ByteArrayInputStream(encryptedWrite.toByteArray)
+    CryptoUtils.decryptAes(encryptedRead, passPhrase, salt, iterationCount, iv)(readString(_) === "Hello World!")
+
+    ok
+  }
+
+  "fail on encryption-/decryption-key mismatch" in {
+    val salt = CryptoUtils.generateRandomOctets(8)
+    val iterationCount = 512
+    val iv = CryptoUtils.generateRandomOctets(16)
+
+    val encryptedWrite = new ByteArrayOutputStream()
+    CryptoUtils.encryptAes(encryptedWrite, "secure-password1".toCharArray, salt, iterationCount, iv)(writeString(_, "Hello World!"))
+    val encryptedRead = new ByteArrayInputStream(encryptedWrite.toByteArray)
+    CryptoUtils.decryptAes(encryptedRead, "secure-password2".toCharArray, salt, iterationCount, iv)(readString(_) !== "Hello World!")
+
+    ok
+  }
+
+  def writeString(s: OutputStream, str: String) {
+    s.write(str.getBytes("UTF-8"))
+  }
+
+  def readString(s: InputStream): String = {
+    val temp = new ByteArrayOutputStream()
+    val buffer = new Array[Byte](1024)
+    var done = false
+
+    while (!done) {
+      val read = s.read(buffer, 0, buffer.length)
+      if (read > 0) temp.write(buffer, 0, read) else done = true
+    }
+
+    new String(temp.toByteArray, "UTF-8")
   }
 }
